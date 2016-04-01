6af12d39a6cfaab1276de05747ec40fa2e6e890c
kyla reid <ksreid@telus.net>
2016-03-31 17:38:35 -0700

    overlay
    overlay is working now yay

--- mapGen.js
+++ mapGen.js
@@ -6,19 +6,14 @@ function initMap() {
 
 google.maps.event.addDomListener(window, 'load', initMap);
 
-var canada=new google.maps.LatLng(63.141651, -113.378906);
 
 function initOverlay() {
-var mapProp = {
-  center:canada,
-  zoom:3,
-  };
-  
-var map = new google.maps.Map(document.getElementById('map'),mapProp);
+var mapDiv = document.getElementById('map');
+var map = new google.maps.Map(mapDiv, {center: {lat:63.141651, lng: -113.378906}, zoom: 3});
 
 var BC = new google.maps.Circle({
   center: {lat: 53.726668, lng: -127.647621},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -27,7 +22,7 @@ var BC = new google.maps.Circle({
   });
 var AB = new google.maps.Circle({
   center: {lat: 53.933271, lng: -116.576504},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -36,7 +31,7 @@ var AB = new google.maps.Circle({
   });
 var SK = new google.maps.Circle({
   center: {lat: 52.939916, lng: -106.450864},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -45,7 +40,7 @@ var SK = new google.maps.Circle({
   });
 var MB = new google.maps.Circle({
   center: {lat: 53.760861, lng: -98.813876},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -54,7 +49,7 @@ var MB = new google.maps.Circle({
   });
 var NB = new google.maps.Circle({
   center: {lat: 46.565316, lng: -66.461916},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -63,7 +58,7 @@ var NB = new google.maps.Circle({
   });
 var PE = new google.maps.Circle({
   center: {lat: 46.510712, lng: -63.416814},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -72,7 +67,7 @@ var PE = new google.maps.Circle({
   });
 var NU = new google.maps.Circle({
   center: {lat: 70.299771, lng: -83.107577},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -81,7 +76,7 @@ var NU = new google.maps.Circle({
   });
 var NL = new google.maps.Circle({
   center: {lat: 53.135509, lng: -57.660436},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -90,7 +85,7 @@ var NL = new google.maps.Circle({
   });
 var NT = new google.maps.Circle({
   center: {lat: 64.825544, lng: -124.845733},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -99,7 +94,7 @@ var NT = new google.maps.Circle({
   });
 var NS = new google.maps.Circle({
   center: {lat: 44.681987, lng: -63.744311},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -108,7 +103,7 @@ var NS = new google.maps.Circle({
   });
 var ON = new google.maps.Circle({
   center: {lat: 51.253775, lng: -85.323214},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -117,7 +112,7 @@ var ON = new google.maps.Circle({
   });
 var QC = new google.maps.Circle({
   center: {lat: 52.939916, lng: -73.549136},
-  radius:20000,
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
@@ -125,8 +120,8 @@ var QC = new google.maps.Circle({
   fillOpacity:0.4
   });
 var YT = new google.maps.Circle({
-  position: {lat: 64.282327, lng: -135.000000},
-  radius:20000,
+  center: {lat: 64.282327, lng: -135.000000},
+  radius:50000,
   strokeColor:"#0000FF",
   strokeOpacity:0.8,
   strokeWeight:2,
