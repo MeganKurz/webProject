45c6247ae9ce85cb4ad022dde23f0a7faf27cd5e
Megan Kurz <meganruthkurz@gmail.com>
2016-04-04 17:59:37 -0700

    Fixed the php call so it actually returns data
    Changed the mysql calls

--- loadmap.php
+++ loadmap.php
@@ -8,7 +8,7 @@ if(!isset($results['error'])){
     $results['result'] = getData($_GET['arguments'][0],$_GET['arguments'][1],$_GET['arguments'][2]);
 }
 echo json_encode($results);
-
+exit;
 
 function getData($province, $foods, $drinks){
 $mysqli = mysqli_connect("localhost", "root", "marbles", "webProject");
@@ -16,15 +16,15 @@ $prov = $province;
 $food = $foods;
 $drink = $drinks;
 if($food =="true" && $drink =="true"){
- $sql = "SELECT food.item, liquids.item FROM food INNER JOIN liquids "
-         . "ON food.prov=liquids.prov WHERE food.prov=$prov"; 
+ $sql = "SELECT food.item, liquids.name FROM food INNER JOIN liquids "
+         . "ON food.prov=liquids.prov WHERE food.prov='$prov'"; 
 }
 else if($food == "true" && $drink == "false"){
-    $sql = "SELECT item FROM food WHERE prov = $prov";
+    $sql = "SELECT item FROM food WHERE prov = '$prov'";
 }
 else{
-    $sql = "SELECT item FROM liquids WHERE prov = $prov";
+    $sql = "SELECT name FROM liquids WHERE prov = '$prov'";
 }
-$sqlResults = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+$sqlResults = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
 return $sqlResults;
 }
--- mapGen.js
+++ mapGen.js
@@ -10,197 +10,172 @@ google.maps.event.addDomListener(window, 'load', initMap);
 
 
 function initOverlay() {
-    var mapDiv = document.getElementById('map');
-    map = new google.maps.Map(mapDiv, {center: {lat: 63.141651, lng: -113.378906}, zoom: 3});
+var mapDiv = document.getElementById('map');
+map = new google.maps.Map(mapDiv, {center: {lat:63.141651, lng: -113.378906}, zoom: 3});
 
-    var BC = new google.maps.Circle({
-        center: {lat: 53.726668, lng: -127.647621},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var AB = new google.maps.Circle({
-        center: {lat: 53.933271, lng: -116.576504},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var SK = new google.maps.Circle({
-        center: {lat: 52.939916, lng: -106.450864},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var MB = new google.maps.Circle({
-        center: {lat: 53.760861, lng: -98.813876},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var NB = new google.maps.Circle({
-        center: {lat: 46.565316, lng: -66.461916},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var PE = new google.maps.Circle({
-        center: {lat: 46.510712, lng: -63.416814},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var NU = new google.maps.Circle({
-        center: {lat: 70.299771, lng: -83.107577},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var NL = new google.maps.Circle({
-        center: {lat: 53.135509, lng: -57.660436},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var NT = new google.maps.Circle({
-        center: {lat: 64.825544, lng: -124.845733},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var NS = new google.maps.Circle({
-        center: {lat: 44.681987, lng: -63.744311},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var ON = new google.maps.Circle({
-        center: {lat: 51.253775, lng: -85.323214},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var QC = new google.maps.Circle({
-        center: {lat: 52.939916, lng: -73.549136},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
-    var YT = new google.maps.Circle({
-        center: {lat: 64.282327, lng: -135.000000},
-        radius: 90000,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#0000FF",
-        fillOpacity: 0.4
-    });
+var BC = new google.maps.Circle({
+  center: {lat: 53.726668, lng: -127.647621},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var AB = new google.maps.Circle({
+  center: {lat: 53.933271, lng: -116.576504},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var SK = new google.maps.Circle({
+  center: {lat: 52.939916, lng: -106.450864},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var MB = new google.maps.Circle({
+  center: {lat: 53.760861, lng: -98.813876},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var NB = new google.maps.Circle({
+  center: {lat: 46.565316, lng: -66.461916},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var PE = new google.maps.Circle({
+  center: {lat: 46.510712, lng: -63.416814},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var NU = new google.maps.Circle({
+  center: {lat: 70.299771, lng: -83.107577},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var NL = new google.maps.Circle({
+  center: {lat: 53.135509, lng: -57.660436},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var NT = new google.maps.Circle({
+  center: {lat: 64.825544, lng: -124.845733},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var NS = new google.maps.Circle({
+  center: {lat: 44.681987, lng: -63.744311},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var ON = new google.maps.Circle({
+  center: {lat: 51.253775, lng: -85.323214},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var QC = new google.maps.Circle({
+  center: {lat: 52.939916, lng: -73.549136},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
+var YT = new google.maps.Circle({
+  center: {lat: 64.282327, lng: -135.000000},
+  radius:50000,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.8,
+  strokeWeight:2,
+  fillColor:"#0000FF",
+  fillOpacity:0.4
+  });
 
-    BC.setMap(map);
-    AB.setMap(map);
-    SK.setMap(map);
-    MB.setMap(map);
-    NB.setMap(map);
-    PE.setMap(map);
-    NU.setMap(map);
-    NL.setMap(map);
-    NT.setMap(map);
-    NS.setMap(map);
-    ON.setMap(map);
-    QC.setMap(map);
-    YT.setMap(map);
+BC.setMap(map);
+AB.setMap(map);
+SK.setMap(map);
+MB.setMap(map);
+NB.setMap(map);
+PE.setMap(map);
+NU.setMap(map);
+NL.setMap(map);
+NT.setMap(map);
+NS.setMap(map);
+ON.setMap(map);
+QC.setMap(map);
+YT.setMap(map);
 
-    BC.addListener('click', function () {
-        sendLocation("BC", {lat: 53.726668, lng: -127.647621});
-    });
-    AB.addListener('click', function () {
-        sendLocation("AB", {lat: 53.933271, lng: -116.576504});
-    });
-    SK.addListener('click', function () {
-        sendLocation("SK", {lat: 52.939916, lng: -106.450864});
-    });
-    MB.addListener('click', function () {
-        sendLocation("MB", {lat: 53.760861, lng: -98.813876});
-    });
-    NB.addListener('click', function () {
-        sendLocation("NB", {lat: 46.565316, lng: -66.461916});
-    });
-    PE.addListener('click', function () {
-        sendLocation("PE", {lat: 46.510712, lng: -63.416814});
-    });
-    NU.addListener('click', function () {
-        sendLocation("NU", {lat: 70.299771, lng: -83.107577});
-    });
-    NL.addListener('click', function () {
-        sendLocation("NL", {lat: 64.825544, lng: -124.845733});
-    });
-    NT.addListener('click', function () {
-        sendLocation("NT", {lat: 64.825544, lng: -124.845733});
-    });
-    NS.addListener('click', function () {
-        sendLocation("NS", {lat: 44.681987, lng: -63.744311});
-    });
-    ON.addListener('click', function () {
-        sendLocation("ON", {lat: 51.253775, lng: -85.323214});
-    });
-    QC.addListener('click', function () {
-        sendLocation("QC", {lat: 52.939916, lng: -73.549136});
-    });
-    YT.addListener('click', function () {
-        sendLocation("YT", {lat: 64.282327, lng: -135.000000});
-    });
+BC.addListener('onclick', sendLocation("BC", {lat: 53.726668, lng: -127.647621}));
+AB.addListener('onclick', sendLocation("AB", {lat: 53.933271, lng: -116.576504}));
+SK.addListener('onclick', sendLocation("SK", {lat: 52.939916, lng: -106.450864}));
+MB.addListener('onclick', sendLocation("MB", {lat: 53.760861, lng: -98.813876}));
+NB.addListener('onclick', sendLocation("NB", {lat: 46.565316, lng: -66.461916}));
+PE.addListener('onclick', sendLocation("PE", {lat: 46.510712, lng: -63.416814}));
+NU.addListener('onclick', sendLocation("NU", {lat: 70.299771, lng: -83.107577}));
+NL.addListener('onclick', sendLocation("NL", {lat: 64.825544, lng: -124.845733}));
+NT.addListener('onclick', sendLocation("NT", {lat: 64.825544, lng: -124.845733}));
+NS.addListener('onclick', sendLocation("NS", {lat: 44.681987, lng: -63.744311}));
+ON.addListener('onclick', sendLocation("ON", {lat: 51.253775, lng: -85.323214}));
+QC.addListener('onclick', sendLocation("QC", {lat: 52.939916, lng: -73.549136}));
+YT.addListener('onclick', sendLocation("YT", {lat: 64.282327, lng: -135.000000}));
 
 }
+
 function sendLocation(province, location){
     var results;
     var food = document.getElementById("food").checked;
     var drink = document.getElementById("drink").checked;
-    jQuery.ajax({
-       type: "GET",
-       url: "loadmap.php",
+    $.ajax({
+       type: 'GET',
+       url: 'loadmap.php',
        dataType: 'json',
        data: {functionname: 'getData', arguments:[province, food, drink]},
-       success: function(data, textstatus){
-           if(!('error' in data)){
-               results = data.result;
+       success: function(feedback){
+           if(!('error' in feedback)){
+               results = feedback.result;
            }
            else{
-               window.alert(data.error);
+               window.alert(feedback.error);
            }
        }
     });
@@ -225,3 +200,11 @@ function sendLocation(province, location){
     infowindow.open(map,province);
 
 }
+
+
+
+
+
+
+
+
